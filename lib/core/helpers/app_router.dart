ce531dc89b7c5995bb2daa406f48ccb4e5a53e47
Heba Elbastawisy <elbastawisyheba@gmail.com>
2025-03-29 14:04:18 +0100

    creating goRouter and splash view folder

---
+++ lib/core/helpers/app_router.dart
@@ -0,0 +1,11 @@
+import 'package:fruit_hub/features/splash/presentation/view/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static final router = GoRouter(routes: [
+    GoRoute(
+      path: "/",
+      builder: (context, state) => const SplashView(),
+    ),
+  ]);
+}
